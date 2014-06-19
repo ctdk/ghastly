67bab1b8e14523afad0d42718ab9459aca268c9c
ctdk <jbingham@gmail.com>
2014-06-18 21:25:09 -0700

    Start filling in domain functions

--- domain.go
+++ domain.go
@@ -13,6 +13,12 @@ type Domain struct {
 	version   *Version
 }
 
+type DomainCheck struct {
+	*Domain
+	Cname string
+	IsProper bool
+}
+
 // Create a new domain for a particular version of a service. Possible parameters
 // are "name" and "comment".
 func (v *Version) NewDomain(params map[string]string) (*Domain, error) {
@@ -27,6 +33,37 @@ func (v *Version) NewDomain(params map[string]string) (*Domain, error) {
 	}
 
 	return v.populateDomain(dData)
+}
+
+// Check all domains associated with a version of a service.
+func (v *Version) CheckAllDomains() ([]*DomainCheck, error) {
+
+}
+
+// Check one domain associated with a service.
+func (v *Version) CheckDomain(name string) (*DomainCheck, error) {
+
+}
+
+// List all domains associated with a version of a service.
+func (v *Version) ListDomains() ([]*Domain, error) {
+
+}
+
+// Get a domain associated with this version
+func (v *Version) GetDomain(name string) (*Domain, error) {
+
+}
+
+// Delete a domain, for the version the domain belongs to.
+func (d *Domain)Delete() error {
+
+}
+
+// Update a domain, for the version the domain belongs to. Possible parameters
+// for the domain are "name" and "comment".
+func (d *Domain)Update(params map[string]string) error {
+
 }
 
 func (v *Version) populateDomain(domainData map[string]interface{}) (*Domain, error) {
